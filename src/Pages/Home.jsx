1362e86a2f3a4f5f19348cb98a7211e150c55a48
arianecrestani <ariane.mrv@gmail.com>
2023-03-22 22:32:51 +0100

    new api for this project

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,45 +5,39 @@ import { useEffect, useState } from "react";
 const Home = () => {
   const [dentists, setDentists] = useState([]);
 
-  const getApiData = async (
-    term,
-    location,
-    categories,
-    sort_by,
-    price,
-    open_now,
-    attributes
-  ) => {
-    const apikey =
-      "DH92QGA5D6MZ90_BUPFU1YFzrXMhIKugwfMvlHXZ2NLzcx7r5A0-iRABaEV541QfRvr_zlg_ZxUxFpJR0Op_58SbfuO6ui_3OmvIPXtCyI_qUOHqndHh9yRu9CMaZHYx";
-    const url = `https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&categories=${categories}&sort_by=${sort_by}&price=${price}&open_now=${open_now}&attributes=${attributes}`;
-    const urlv =
-      "https://api.yelp.com/v3/businesses/search?term=dentists&location=berlin&categories=dentists&sort_by=rating&locale=de_DE";
-    const myHeaders = new Headers();
-    myHeaders.append("Authorization", "Bearer DH92QGA5D6MZ90_BUPFU1YFzrXMhIKugwfMvlHXZ2NLzcx7r5A0-iRABaEV541QfRvr_zlg_ZxUxFpJR0Op_58SbfuO6ui_3OmvIPXtCyI_qUOHqndHh9yRu9CMaZHYx");
+  const [loading, setLoading] = useState(false);
+  const getApiData = async () => {
+    const testUrl = `https://overpass-api.de/api/interpreter?data=[out:json];node(around:3000,52.516275,13.377704)[amenity=dentist];out;`;
 
-    const requestOptions = {
-      method: "GET",
-      headers: myHeaders,
-      redirect: "follow",
-    };
-    try {
-      const response = await fetch(urlv, requestOptions);
-      const data = await response.json();
-      setDentists(data.businesses);
-      console.log(data.businesses);
-    } catch (error) {
-      console.log(error)
-    }
+    fetch(testUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        setLoading(false);
+        setDentists(data.elements);
+      })
+      .catch((error) => {
+        console.error(error);
+        setLoading(false);
+      });
   };
 
   useEffect(() => {
-    getApiData("dentists", "berlin", "dentists", "rating", "de_DE");
+    setLoading(true);
+
+    getApiData();
   }, []);
 
   return (
     <div>
-      <div>{dentists && dentists.map((item) => <li>{item.alias}</li>)}</div>
+      {loading && <p>loading</p>}
+      <div>
+        {dentists.map((item) => (
+          <li>
+            {item.tags.amenity} {item.tags.name} --- {item.tags["addr:street"]}
+          </li>
+        ))}
+      </div>
+
       <div>
         <Link to={`/login`}>Login</Link>
         <Stack spacing={3}></Stack>
