4686341a5622cb2e238dacaf1703d1364f164159
arianecrestani <ariane.mrv@gmail.com>
2023-04-03 17:43:21 +0200

    set style for each card

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Box, HStack } from "@chakra-ui/react";
+import { Box, HStack, Flex, Text, Spacer } from "@chakra-ui/react";
 import { AuthContext } from "../Contexts/AuthContext";
 import { useContext } from "react";
 import { UserArea } from "../Pages/UserArea";
@@ -11,25 +11,31 @@ const Navbar = () => {
   //para cada id tera uma lista de review
 
   return (
-    <Box display="flex">
-      <HStack padding={10} spacing={20}>
-        <Link to={`/`}>Home</Link>
+    <Box bg="white" color="blue.500">
+      <Flex padding={5} alignItems="center">
+        <Text fontSize="xl" fontWeight="bold">
+          Dentist Web Page
+        </Text>
+        <Spacer />
+        <Box display="flex">
+          <HStack spacing={10}>
+            <Link to={`/`}>Home</Link>
 
-        {user ? (
-          <>
-            <button onClick={logOut}>Logout</button>
-            <p>{user.email}</p>
-            <Link to="/login">
-              user
-            </Link>
-          </>
-        ) : (
-          <>
-            <Link to={`/register`}>Register</Link>
-            <Link to="/login">Log in</Link>
-          </>
-        )}
-      </HStack>
+            {user ? (
+              <>
+                <button onClick={logOut}>Logout</button>
+                <p>{user.email}</p>
+                <Link to="/user">User</Link>
+              </>
+            ) : (
+              <>
+                <Link to={`/register`}>Register</Link>
+                <Link to="/login">Log in</Link>
+              </>
+            )}
+          </HStack>
+        </Box>
+      </Flex>
     </Box>
   );
 };
